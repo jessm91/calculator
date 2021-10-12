36b5bdd5caa47a82ee93c4841df0997fd7682b7a
Jessica Mangiameli <jmangiam@asu.edu>
2021-10-12 09:33:11 -0500

    added quit function

--- calculator.py
+++ calculator.py
@@ -3,12 +3,12 @@ print("Python Calculator [enter 'q' to quit]")
 while True:
     function = input("Would you like to add, subtract, multiply or divide? Please choose one: ")
 
+    if function == 'q':
+        break
+        
 #User enters 2 numbers
     first_numb = input("Enter the first number: ")
     sec_numb = input("Enter the second number: ")
-
-    if function == 'q' or first_numb == 'q' or sec_numb == 'q':
-        break
 
 #addition, subtraction, multiply, divide
     if function == 'add':
