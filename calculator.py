24e4c767e7544a21331f01c5028cf13b0425cb35
Jessica Mangiameli <jmangiam@asu.edu>
2021-10-12 09:20:55 -0500

    multiply and divide

--- calculator.py
+++ calculator.py
@@ -1,5 +1,20 @@
+function =
+input("Would you like to add, subtract, multiply or divide? Please choose one: ")
+
+#User enters 2 numbers
 first_numb = input("Enter the first number: ")
 sec_numb = input("Enter the second number: ")
 
-answer = int(first_numb) + int(sec_numb)
-print(answer)
+#addition, subtraction, multiply, divide
+if function == 'add':
+    answer = int(first_numb) + int(sec_numb)
+    print(answer)
+elif function == 'subtract':
+    answer = int(first_numb) - int(sec_numb)
+    print(answer)
+elif function == 'multiply':
+    answer = int(first_numb) - int(sec_numb)
+    print(answer)
+elif function == 'divide':
+    answer = int(first_numb) - int(sec_numb)
+    print(answer)
