8097451885b266762cba0ae6b0ed07d3bbfe598b
Jessica Mangiameli <jmangiam@asu.edu>
2021-10-12 09:26:06 -0500

    added while loop fixed operators

--- calculator.py
+++ calculator.py
@@ -1,20 +1,25 @@
-function =
-input("Would you like to add, subtract, multiply or divide? Please choose one: ")
+print("Python Calculator [enter 'q' to quit]")
+
+while True:
+    function = input("Would you like to add, subtract, multiply or divide? Please choose one: ")
 
 #User enters 2 numbers
-first_numb = input("Enter the first number: ")
-sec_numb = input("Enter the second number: ")
+    first_numb = input("Enter the first number: ")
+    sec_numb = input("Enter the second number: ")
+
+    if function == 'q' or first_numb == 'q' or sec_numb == 'q':
+        break
 
 #addition, subtraction, multiply, divide
-if function == 'add':
-    answer = int(first_numb) + int(sec_numb)
-    print(answer)
-elif function == 'subtract':
-    answer = int(first_numb) - int(sec_numb)
-    print(answer)
-elif function == 'multiply':
-    answer = int(first_numb) - int(sec_numb)
-    print(answer)
-elif function == 'divide':
-    answer = int(first_numb) - int(sec_numb)
-    print(answer)
+    if function == 'add':
+        answer = int(first_numb) + int(sec_numb)
+        print(answer)
+    elif function == 'subtract':
+        answer = int(first_numb) - int(sec_numb)
+        print(answer)
+    elif function == 'multiply':
+        answer = int(first_numb) * int(sec_numb)
+        print(answer)
+    elif function == 'divide':
+        answer = int(first_numb) / int(sec_numb)
+        print(answer)
