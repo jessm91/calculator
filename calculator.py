6822af3c2cb94c2a06d066eb273a893d5f549c1f
Jessica Mangiameli <jmangiam@asu.edu>
2021-10-12 10:04:56 -0500

    divisable by 0

--- calculator.py
+++ calculator.py
@@ -1,25 +1,46 @@
 print("Python Calculator [enter 'q' to quit]")
 
 while True:
+    #user input
     function = input("Would you like to add, subtract, multiply or divide? Please choose one: ")
 
     if function == 'q':
         break
-        
-#User enters 2 numbers
+
     first_numb = input("Enter the first number: ")
     sec_numb = input("Enter the second number: ")
 
 #addition, subtraction, multiply, divide
     if function == 'add':
-        answer = int(first_numb) + int(sec_numb)
-        print(answer)
+        try:
+            answer = int(first_numb) + int(sec_numb)
+        except ValueError:
+            print("Please enter numbers only!")
+        else:
+            print(answer)
     elif function == 'subtract':
-        answer = int(first_numb) - int(sec_numb)
-        print(answer)
+        try:
+            answer = int(first_numb) - int(sec_numb)
+        except ValueError:
+            print("Please enter numbers only!")
+        else:
+            print(answer)
     elif function == 'multiply':
-        answer = int(first_numb) * int(sec_numb)
-        print(answer)
+        try:
+            answer = int(first_numb) * int(sec_numb)
+        except ValueError:
+            print("Please enter numbers only!")
+        else:
+            print(answer)
     elif function == 'divide':
-        answer = int(first_numb) / int(sec_numb)
-        print(answer)
+        try:
+            if int(first_numb) == 0:
+                answer = 0
+            elif int(sec_numb) == 0:
+                answer = "Error"
+            else:
+                answer = int(first_numb) / int(sec_numb)
+        except ValueError:
+            print("Please enter numbers only!")
+        else:
+            print(answer)
